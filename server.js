5d5fe68026888c754d1a293b4cda5478c04e05a0
dear.avana <dear.avana@gmail.com>
2021-02-12 14:20:51 -0500

    Forgot to update version number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /**
  *  @file Express server with IP validation middleware and graceful cleanup
  *  @author Avana Vana <dear.avana@gmail.com>
- *  @version 1.6.1
+ *  @version 1.7.0
  */
 
 const dotenv = require('dotenv').config();
