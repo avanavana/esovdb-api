03f35a73876ee829b2e2dd4ee7f884206b752cc6
dear.avana <dear.avana@gmail.com>
2025-02-22 03:26:06 -0500

    fix(youtube): esovdb.processAdditions is not async
    This method, which wraps the old-fashioned Airtable API, uses the old
    callback method of running async tasks, and does not return a promise,
    therefore cannot be awaited.

--- youtube.js
+++ youtube.js
@@ -205,7 +205,7 @@ const processChannelVideos = async (channelId, length, publishedAfter) => {
       }
     }));
 
-    const data = await esovdb.processAdditions(itemsToAdd, 'Submissions');
+    const data = esovdb.processAdditions(itemsToAdd, 'Submissions');
     return { status: 201, data: { ...data, channel: videos[0].channel, channelId: videos[0].channelId }}
   } catch (error) {
     console.error(`Error processing videos from YouTube channel ${channelId}:`, error)
